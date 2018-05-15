283b91b30b35300226fa76d6b0031f157a1f2c33
675435543 <675435543@qq.com>
2018-05-16 06:59:52 +0800

    helloworld

---
+++ springboot-datatables-datagrid/src/main/java/com/forezp/entity/P2pLoanTasklog.java
@@ -0,0 +1,110 @@
+package com.forezp.entity;
+
+public class P2pLoanTasklog {
+	private String tasklogId;
+	private String loanName;
+	private String custId;
+	private String userName;
+	private String realName;
+	private String creditGrade;
+	private String serviceId;
+	private String serviceName;
+	private String profitInterest;
+	private String loanDeadline;
+	private String loanDeadlineType;
+	private String createTime;
+	private String finishTime;
+	private String status;
+	private String updateTime;
+	public String getTasklogId() {
+		return tasklogId;
+	}
+	public void setTasklogId(String tasklogId) {
+		this.tasklogId = tasklogId;
+	}
+	public String getLoanName() {
+		return loanName;
+	}
+	public void setLoanName(String loanName) {
+		this.loanName = loanName;
+	}
+	public String getCustId() {
+		return custId;
+	}
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getRealName() {
+		return realName;
+	}
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+	public String getCreditGrade() {
+		return creditGrade;
+	}
+	public void setCreditGrade(String creditGrade) {
+		this.creditGrade = creditGrade;
+	}
+	public String getServiceId() {
+		return serviceId;
+	}
+	public void setServiceId(String serviceId) {
+		this.serviceId = serviceId;
+	}
+	public String getServiceName() {
+		return serviceName;
+	}
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+	public String getProfitInterest() {
+		return profitInterest;
+	}
+	public void setProfitInterest(String profitInterest) {
+		this.profitInterest = profitInterest;
+	}
+	public String getLoanDeadline() {
+		return loanDeadline;
+	}
+	public void setLoanDeadline(String loanDeadline) {
+		this.loanDeadline = loanDeadline;
+	}
+	public String getLoanDeadlineType() {
+		return loanDeadlineType;
+	}
+	public void setLoanDeadlineType(String loanDeadlineType) {
+		this.loanDeadlineType = loanDeadlineType;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public String getFinishTime() {
+		return finishTime;
+	}
+	public void setFinishTime(String finishTime) {
+		this.finishTime = finishTime;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+
+}
