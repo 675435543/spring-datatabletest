3810d268446257e72e154b10c48f135f333b08db
liudada <675435543@qq.com>
2018-09-26 18:54:26 +0800

    Update Constants.java

--- springboot-datatables-datagrid/src/main/java/com/forezp/utils/Constants.java
+++ springboot-datatables-datagrid/src/main/java/com/forezp/utils/Constants.java
@@ -1,5 +1,542 @@
 package com.forezp.utils;
 
-public class Constants {
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * 常量
+ * 
+ * 
+ *
+ */
+public final class Constants
+{
+    private Constants()
+    {
+
+    }
+
+    /**
+     * 存放在session的用户id信息的key名字
+     */
+    public static final String SESSION_RDSEARCH_USER = "se_rdsearchuser";
+
+    /**
+     * 存放在session的用户上次搜索collection
+     */
+    public static final String SESSION_RDSEARCH_LAST_COLLECTION = "se_rdsearchlastcollection";
+
+    /**
+     * 存放在session的用户role信息的key名字
+     */
+    public static final String SESSION_RDSEARCH_ROLE = "se_rdsearchrole";
+
+    /**
+     * 存放在session的用户是否需要用户引导状态
+     */
+    public static final String SESSION_RDSEARCH_GUIDE = "se_rdsearchguide";
+
+    /**
+     * 存放在session的全局条件
+     */
+    public static final String SESSION_RDSEARCH_CONDITION = "se_rdsearch_condition";
+
+    /**
+     * 存放在session的请求id
+     */
+    public static final String SESSION_RDSEARCH_UUID = "se_rdsearch_uuid";
+
+    /**
+     * 存放在session的用户配置的lable
+     */
+    public static final String SESSION_RDSEARCH_USER_FOLLOWLABELS = "se_rdsearchuser_followLabels";
+
+    /**
+     * 存放在session的用户相关的人
+     */
+    public static final String SESSION_RDSEARCH_USER_RELATIONSHIP = "se_rdsearchuser_relationship";
+
+    /**
+     * 存放在session的用户的职级信息
+     */
+    public static final String SESSION_RDSEARCH_USER_LEVEL = "se_rdsearchuser_level";
+
+    /**
+     * 存放在session的用户的类别信息
+     */
+    public static final String SESSION_RDSEARCH_USER_CATEGORY = "se_rdsearchuser_category";
+
+    /**
+     * 存放在session的用户的权限信息
+     */
+    public static final String SESSION_RDSEARCH_USER_RIGHTS = "se_rdsearchuser_rights";
+
+    /**
+     * 存放在session的用户标签(通过离线计算得出)
+     */
+    public static final String SESSION_RDSEARCH_USER_COMPUTED_LABELS = "se_rdsearchuser_computedLabels";
+
+    /**
+     * 存放在session的用户的自定义可选标签
+     */
+    public static final String SESSION_RDSEARCH_USER_OPTIONLABELS = "se_rdsearchuser_optionlabels";
+
+    /**
+     * 存放在session的查询缓存 query
+     */
+    public static final String SESSION_RDSEARCH_QUERY_CACHE = "se_rdsearch_query_cache";
+
+    /**
+     * 存放在session的查询缓存 experts
+     */
+    public static final String SESSION_RDSEARCH_QUERY_EXPERTS_CACHE = "se_rdsearch_query_experts_cache";
+
+    /**
+     * 存放在session的查询缓存 facet
+     */
+    public static final String SESSION_RDSEARCH_QUERY_FACET_CACHE = "se_rdsearch_query_facet_cache";
+    /**
+     * 存放在session的page查询缓存
+     */
+    public static final String SESSION_RDSEARCH_QUERY_PAGE_CACHE = "se_rdsearch_query_page_cache";
+
+    /**
+     * 存放session的omtype查询缓存
+     */
+    public static final String SESSION_RDSEARCH_QUERY_OMTYPE_CACHE = "se_rdsearch_query_page_omtype_cache";
+
+    /**
+     * 以用户为单位存放在session中的配置缓存
+     */
+    public static final String SESSION_RDSEARCH_USER_CONFIG_CACHE = "se_rdsearch_user_config_cache";
+
+    /**
+     * 存放在cookie中的过期时间
+     */
+    public static final String COOKIE_TIME = "cookie_cookieTime";
+
+    /**
+     * cookie默认过期时间
+     */
+    public static final int COOKIE_DEF_EXP_TIEM = 20 * 60 * 1000;
+
+    /**
+     * 存放在cookie的用户id信息的key名字
+     */
+    public static final String COOKIE_NAME = "cookie_rdsearchuser";
+
+    //  记录用户操作
+    /**
+     * 用户对本次查询结果满意
+     */
+    public static final String OP_SATISFY = "satisfied";
+
+    /**
+     * 用户对本次查询结果不满意
+     */
+    public static final String OP_NOT_SATISFY = "dissatisfied";
+
+    /**
+     * 用户对本条记录点击赞同
+     */
+    public static final String OP_PRAISE = "praise";
+
+    /**
+     * 用户对本条记录报错
+     */
+    public static final String OP_MODIFY = "modify";
+
+    /**
+     * 用户对本条记录收藏
+     */
+    public static final String OP_COLLECT = "collect";
+
+    /**
+     * 用户对本条记录报告
+     */
+    public static final String OP_REPORT = "report";
+
+    /**
+     * employee信息分隔符
+     */
+    public static final String NAME_SPLIT = "@#";
+
+    /**
+     * employee信息分隔符
+     */
+    public static final String ID_SUFFIX = "_app";
+
+    /**
+     * collection join 查询
+     */
+    public static final String JOIN_USERSTAT = "join_userStat";
+
+    /**
+     * 1.系统管理员（即之前所说的管理员，可以查看所有：pdu，工具，通用
+     */
+    public static final String CFG_ROLE_1 = "系统管理员";
+
+    /**
+     * 2.运营管理员（可以查看所有pdu）
+     */
+    public static final String CFG_ROLE_2 = "运营管理员";
+
+    /**
+     * 3.知识经理（权限部分填写pdu名）
+     */
+    public static final String CFG_ROLE_3 = "知识经理";
+
+    /**
+     * 0.沒有配置的權限
+     */
+    public static final String CFG_ROLE_0 = "anonymous";
+
+    /**
+     * "部门"
+     */
+    public static final String CFG_TREE_ROOT_1 = "部门";
+
+    /**
+     * "工具"
+     */
+    public static final String CFG_TREE_ROOT_2 = "工具";
+
+    /**
+     * "通用"
+     */
+    public static final String CFG_TREE_ROOT_3 = "通用";
+
+    /**
+     * 1.数据源 svn
+     */
+    public static final String RESOURCEFROM_SVN = "svn";
+
+    /**
+     * 2.数据源 smb
+     */
+    public static final String RESOURCEFROM_SMB = "smb";
+
+    /**
+     * 3.数据源 hi3ms
+     */
+    public static final String RESOURCEFROM_HI3MS = "hi3ms";
+
+    /**
+     * 4.数据源 ipal
+     */
+    public static final String RESOURCEFROM_IPAL = "ipal";
+
+    /**
+     * 5.数据源 dts
+     */
+    public static final String RESOURCEFROM_DTS = "dts";
+
+    /**
+     * 6.数据源 dbox
+     */
+    public static final String RESOURCEFROM_DBOX = "dbox";
+
+    /**
+     * 7.数据源 omsys
+     */
+    public static final String RESOURCEFROM_OMSYS = "omsys";
+
+    /**
+     * 8.数据源 案例
+     */
+    public static final String RESOURCEFROM_CASE = "案例";
+
+    /**
+     * 9.数据源 icare
+     */
+    public static final String RESOURCEFROM_ICARE = "icare";
+
+    /**
+     * 10.数据源 jira
+     */
+    public static final String RESOURCEFROM_JIRA = "jira";
+
+    /**
+     * 11.数据源 uniom
+     */
+    public static final String RESOURCEFROM_UNIOM = "uniom";
+
+    /**
+     * 12.数据源 ecare
+     */
+    public static final String RESOURCEFROM_ECARE = "ecare";
+
+    /**
+     * hi3团队空间前缀
+     */
+    public static final String HI3MS_TEAMSPACE_PREFIX = "http://3ms.huawei.com/hi/group/";
+
+    /**
+     * hi3个人博客
+     */
+    public static final String HI3MS_PERSONALBLOG = "http://3ms.huawei.com/km/blogs/{uid}/list";
+
+    /**
+     * 14.数据源 codeclub
+     */
+    public static final String RESOURCEFROM_CODECLUB = "codeclub";
+
+    /**
+     * 15.数据源 codeclub wiki
+     */
+    public static final String RESOURCEFROM_CODECLUB_WIKI_1 = "1";
+    /**
+     * 16.数据源 codeclub issues
+     */
+    public static final String RESOURCEFROM_CODECLUB_ISSUES_2 = "2";
+    /**
+     * 17.数据源 codeclub wiki
+     */
+    public static final String RESOURCEFROM_CODECLUB_WIKI = "wiki";
+    /**
+     * 18.数据源 codeclub issues
+     */
+    public static final String RESOURCEFROM_CODECLUB_ISSUES = "issues";
+    /**
+     * 19.数据源 isource
+     */
+    public static final String RESOURCEFROM_ISOURCE = "isource";
+    /**
+     * 20.数据源 isource_project
+     */
+    public static final String RESOURCEFROM_ISOURCE_PROJECT = "project";
+    /**
+     * 21.数据源 isource_issues
+     */
+    public static final String RESOURCEFROM_ISOURCE_ISSUES = "issues";
+    /**
+     * 21.数据源 isource_wiki
+     */
+    public static final String RESOURCEFROM_ISOURCE_WIKI = "wiki";
+    /**
+     * 22.数据源 isource project
+     */
+    public static final String RESOURCEFROM_ISOURCE_PROJECT_1 = "1";
+    /**
+     * 23.数据源 isource issues
+     */
+    public static final String RESOURCEFROM_ISOURCE_ISSUES_2 = "2";
+    /**
+     * 24.数据源 isource wiki
+     */
+    public static final String RESOURCEFROM_ISOURCE_WIKI_3 = "3";
+    /**
+     * 25.数据源 wireless_alm
+     */
+    public static final String RESOURCEFROM_WIRELESS_ALM = "wireless_alm";
+    /**
+     * 26.数据源 support
+     */
+    public static final String RESOURCEFROM_SUPPORT = "support";
+
+    /**
+     * 数据抓取对应roleid
+     */
+    @SuppressWarnings("serial")
+    public static final Map<String, Integer> ROLEID_MAP = Collections.unmodifiableMap(new HashMap<String, Integer>()
+    {
+        {
+            put("codeClub", NumConstants.NUM_83);
+            put("svn", NumConstants.NUM_6);
+            put("smb", NumConstants.NUM_6);
+            put("hi3ms", NumConstants.NUM_5);
+            put("isource", NumConstants.NUM_2);
+            put("support", NumConstants.NUM_5);
+        }
+
+    });
+
+    /**
+     * 通用搜索入口
+     */
+    public static final String INDEX_COMMON = "通用搜索入口";
+    /**
+     * QueryItemVO中的similarGroupID属性名
+     */
+    public static final String QUERYITEMVO_SIMILARGROUPID = "similargroupid";
+
+    /**
+     * QueryItemVO中的similarDocNum属性名
+     */
+    public static final String QUERYITEMVO_SIMILARDOCNUM = "similardocnum";
+
+    /**
+     * es引擎
+     */
+    public static final String ENGINE_TYPE_ES = "es";
+
+    /**
+     * solr引擎
+     */
+    public static final String ENGINE_TYPE_SOLR = "solr";
+
+    /**
+     * 邮件服务
+     */
+    public static final String SHORT_MSG_SERVERURL = "shortMsgServerUrl";
+
+    /**
+     * 邮件key
+     */
+    public static final String SHORT_MSG_APPKEY = "shortMsgAppKey";
+
+    /**
+     * phoneNums
+     */
+    public static final String PHONENUMS = "phoneNums";
+
+    /**
+     * warnMailTo
+     */
+    public static final String WARNMAILTO = "warnMailTo";
+
+    /**
+     * warnMailCc
+     */
+    public static final String WARNMAILCC = "warnMailCc";
+
+    /**
+     * 表示为官方搜索
+     */
+    public static final String QUERY_OFFICIAL = "official";
+
+    /**
+     * 表示为热门搜索
+     */
+    public static final String QUERY_HOT = "hot";
+
+    /**
+     * 表示为通用搜索
+     */
+    public static final String QUERY_COMMON = "common";
+
+    /**
+     * 表示为推荐搜索
+     */
+    public static final String QUERY_RECOMMEND = "recommend";
+
+    /**
+     * 表示为全词搜索
+     */
+    public static final String QUERY_WHOLE = "whole";
+
+    /**
+     * solr打分模型中的配置类型： api额外参数
+     */
+    public static final String SOLR_SCORE_TYPE_API_EXT = "apiExtParam";
+
+    /**
+     * solr打分模型中的配置类型： api文本提取
+     */
+    public static final String SOLR_SCORE_TYPE_API_TEXT = "apiText";
+
+    /**
+     * solr打分模型中的配置类型： docSimilarity提取
+     */
+    public static final String SOLR_SCORE_TYPE_DOC_SIMILARITY = "docSimilarity";
+
+    /**
+     * solr打分模型中的配置类型： api查询配置
+     */
+    public static final String SOLR_SCORE_TYPE_API_QUERY = "apiQuery";
+
+    /**
+     * solr打分模型中配置的key：查询排序
+     */
+    public static final String SOLR_SCORE_KEY_QUERY_SORT = "querySort";
+
+    /**
+     * solr打分模型中配置的key：推荐数量 主要api用
+     */
+    public static final String SOLR_SCORE_KEY_RECOMMEND_NUM = "recommendNum";
+
+    /**
+     * solr打分模型中配置的key：全词匹配数量 主要api用
+     */
+    public static final String SOLR_SCORE_KEY_WHOLE_NUM = "wholeNum";
+
+    /**
+     * solr打分模型中配置的key: 超时时间
+     */
+    public static final String SOLR_SCORE_KEY_TIME_OUT = "queryTimeOut";
+
+    /**
+     * solr打分模型中的配置类型：查询校验
+     */
+    public static final String SOLR_SCORE_TYPE_QUERY_VALIDATE = "queryValidate";
+
+    /**
+     * solr打分模型中配置的key: 黑名单
+     */
+    public static final String SOLR_SCORE_KEY_BLACK_LIST = "blackList";
+
+    /**
+     * solr打分模型中配置的key：version，表示数据源是按版本配置
+     */
+    public static final String SOLR_SCORE_KEY_SC_VERSION = "version";
+
+    /**
+     * solr打分模型中配置的key：department，表示数据源是按类型配置
+     */
+    public static final String SOLR_SCORE_KEY_SC_DEPARTMENT = "department";
+
+    /**
+     * solr打分模型中配置的key：pduId，表示数据源是按pduid配置
+     */
+    public static final String SOLR_SCORE_KEY_SC_PDUID = "pduId";
+
+    /**
+     * solr打分模型中配置的key：
+     */
+    public static final String SOLR_SCORE_TYPE_SC_SUB = "sourceConfigSub";
+    /**
+     * 同义词类型(excle导入)
+     */
+    public static final String SYNONYMS_MAPPING = "映射";
+    /**
+     * 同义词类型(excle导入)
+     */
+    public static final String SYNONYMS_GROUP = "组";
+
+    /**
+     * api类型，用于在solr_score_model表中获取查询solr时的fl和fq
+     */
+    public static final String API_TYPE_DOCSIMILARITY = "docSimilarity";
+
+    /**
+     * 共性问题查询优先级权重
+     */
+    public static final String PRIORITY_WEIGHT = "weight";
+
+    /**
+     * 共性问题查询显示数据的最小分数
+     */
+    public static final String MIN_SCORE = "minScore";
+    
+    /**
+     * app相关配置
+     */
+    public static final String SOLR_SCORE_TYPE_APP = "app";
+    
+    /**
+     * cqi配置的key，配置触发关键词
+     */
+    public static final String SOLR_SCORE_KEY_CQI_TERM = "qisCqiTerm";
+    
+    /**
+     * cqi跳转链接的key
+     */
+    public static final String SOLR_SCORE_KEY_CQI_URL = "qisCqiUrl";
+    
+    /**
+     * cqi跳转链接的key
+     */
+    public static final String SOLR_SCORE_KEY_CQI_ORG_URL = "qisCqiOrgUrl";
 }
+
