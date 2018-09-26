0e2d9759919731a91a993aae798402e17fa50b98
liudada <675435543@qq.com>
2018-09-26 18:49:57 +0800

    Update CfgHistoryProcessUtil.java

--- springboot-datatables-datagrid/src/main/java/com/forezp/utils/CfgHistoryProcessUtil.java
+++ springboot-datatables-datagrid/src/main/java/com/forezp/utils/CfgHistoryProcessUtil.java
@@ -1,5 +1,58 @@
 package com.forezp.utils;
 
-public class CfgHistoryProcessUtil {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.log4j.Logger;
+
+import com.huawei.rdsearch.biz.CfgOperationHistoryService;
+import com.huawei.rdsearch.entity.CfgOperationHistory;
+
+/**
+ * 
+ *
+ */
+public class CfgHistoryProcessUtil
+{
+    protected static final Logger LOG = Logger.getLogger(CfgHistoryProcessUtil.class);
+
+    private static final ExecutorService POOL = Executors.newFixedThreadPool(5);
+    
+    private CfgOperationHistory cfgOperationHistory;
+
+    CfgHistoryProcessUtil(CfgOperationHistory cfgOperationHistory)
+    {
+        this.cfgOperationHistory = cfgOperationHistory;
+    }
+
+    /**
+     * 发送操作 记录
+     * @param cfgOperationHistory cfgOperationHistory
+     */
+    public static void recordHistory(CfgOperationHistory cfgOperationHistory)
+    {
+        new CfgHistoryProcessUtil(cfgOperationHistory).start();
+    }
+
+    private void start()
+    {
+        CfgHistoryProcessUtil.POOL.execute(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    ((CfgOperationHistoryService) Util.getBeanByName("cfgOperationHistoryServiceImpl"))
+                            .add(CfgHistoryProcessUtil.this.cfgOperationHistory);
+                }
+                catch (Exception e)
+                {
+                    CfgHistoryProcessUtil.LOG.error("recordHistory error!", e);
+                }
+            }
+        });
+    }
 
 }
+
