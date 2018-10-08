6c3c0bd848fca14724ad6facb0f33405a5cf3ce3
675435543 <675435543@qq.com>
2018-10-09 07:14:58 +0800

    笔记更新

---
+++ springboot-datatables-datagrid/src/main/java/com/forezp/jdksource/test.java
@@ -0,0 +1,10 @@
+package com.forezp.jdksource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+	public static void main(String[] args) {
+		List<String> strList = new ArrayList<String>();
+	}
+}
