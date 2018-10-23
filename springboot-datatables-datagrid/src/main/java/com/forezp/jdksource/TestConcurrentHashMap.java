db770855a6532ebd5fa84bcc0adb986c540e49c3
675435543 <675435543@qq.com>
2018-10-24 07:15:50 +0800

    hello

---
+++ springboot-datatables-datagrid/src/main/java/com/forezp/jdksource/TestConcurrentHashMap.java
@@ -0,0 +1,11 @@
+package com.forezp.jdksource;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TestConcurrentHashMap {
+
+	public static void main(String[] args) {
+		
+		ConcurrentHashMap<String,String> c = new ConcurrentHashMap<String,String>();
+	}
+}
